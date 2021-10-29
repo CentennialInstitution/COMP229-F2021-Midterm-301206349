b8183a812fdf35c9dba80a06671ddb2d497c108f
Abrar Mohammed <mohd.abrar011@gmail.com>
2021-10-28 22:20:04 -0400

    Midterm changes

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://1234:1234@cluster0.vq5ki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
